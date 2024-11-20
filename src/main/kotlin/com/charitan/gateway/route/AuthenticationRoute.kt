9a7f65f96865fee55295af41e3f7041f73a299c3
Nguyen Do <khoanguyen.do@outlook.com>
2024-11-20 13:54:07 +0700

    feat: implement auth service routing

---
+++ src/main/kotlin/com/charitan/gateway/route/AuthenticationRoute.kt
@@ -0,0 +1,22 @@
+package com.charitan.gateway.route
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AuthenticationRoute {
+    @Bean
+    fun authenticationRoutes(
+        builder: RouteLocatorBuilder
+    ): RouteLocator {
+        return builder.routes {
+            route() {
+                path("/api/auth/**")
+                uri("lb://AUTH")
+            }
+        }
+    }
+}
