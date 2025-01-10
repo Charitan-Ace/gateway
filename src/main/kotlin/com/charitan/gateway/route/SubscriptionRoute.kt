eb58aa32d5c03d43dd89abb9734b6a27310d1cd1
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-10 10:02:58 +0700

    add subscription route

---
+++ src/main/kotlin/com/charitan/gateway/route/SubscriptionRoute.kt
@@ -0,0 +1,23 @@
+package com.charitan.gateway.route
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.filters
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SubscriptionRoute {
+    @Bean
+    fun donationRoutes(builder: RouteLocatorBuilder): RouteLocator =
+        builder.routes {
+            route {
+                path("/subscription/**")
+                filters {
+                    this.stripPrefix(1)
+                }
+                uri("lb://SUBSCRIPTION")
+            }
+        }
+}
