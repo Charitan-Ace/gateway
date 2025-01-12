7559e9a84de21def1e6af2757392356457bbb30e
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-12 18:06:00 +0700

    fix: add `@Configuration`

--- src/main/kotlin/com/charitan/gateway/route/StatisticsRoute.kt
+++ src/main/kotlin/com/charitan/gateway/route/StatisticsRoute.kt
@@ -2,10 +2,11 @@ package com.charitan.gateway.route
 
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.cloud.gateway.route.builder.filters
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class StatisticsRoute {
     @Bean
     fun statisticsRoutes(builder: RouteLocatorBuilder): RouteLocator =
