24228cc766cc2d8e6dddbb1bcc01f46e949c4dec
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-13 18:53:19 +0700

    feat: add statement route

---
+++ src/main/kotlin/com/charitan/gateway/route/StatementRoute.kt
@@ -0,0 +1,19 @@
+package com.charitan.gateway.route
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class StatementRoute {
+    @Bean
+    fun statementRoute(builder: RouteLocatorBuilder): RouteLocator =
+        builder.routes {
+            route {
+                path("/api/statement/**")
+                uri("lb://STATEMENT")
+            }
+        }
+}
